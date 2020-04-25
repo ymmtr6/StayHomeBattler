d4237dba1d8bfa7307a8ad79d7d7b80876f6a02f
Riku Yamamoto <ymmtr666@gmail.com>
2020-04-25 23:47:03 +0900

    Initial Commit

---
+++ StayHomeBattler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StayHomeBattler
+//
+//  Created by Riku Yamamoto on 2020/04/25.
+//  Copyright © 2020 Riku Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
